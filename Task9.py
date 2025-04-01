6f111c367269462bf0ddb81ecface7629bf0d5e9
Daniel Prem <danielprem1197@gmail.com>
2025-04-01 10:54:46 +0530

    Update Task9.py

--- Task9.py
+++ Task9.py
@@ -1,20 +1,20 @@
-import time
-
+from time import sleep
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.chrome.service import service
-
-
 
 driver = webdriver.Chrome()
 driver.get("https://www.saucedemo.com/")
-time.sleep(5)
-page_content =driver.find_element(By.XPATH,'/html/body').text
-file = open('webpage_txt11.txt','w')
-file.write(page_content)
-print(driver.title)
-print(driver.current_url)
-print("webpage content saved to 'webpage_txt11.txt'")
-file.close()
-
+user_name = driver.find_element(By.ID,'user-name')
+user_name.send_keys('standard_user')
+password = driver.find_element(By.ID,'password')
+password.send_keys('secret_sauce')
+driver.find_element(By.ID,'login-button').click()
+sleep(5)
+print(driver.title)            #1 Title of the page
+print(driver.current_url)      #2 Current url of the page
+page_content =driver.find_element(By.XPATH,'/html/body').text     #3
+file_obj = open('webpage_task_11.txt', 'w')
+file_obj.write(page_content)     #3. Extracting contents of the webpage and writing it in txt file
+print("webpage content saved to 'webpage_task_11.txt' file!")
+file_obj.close()
 
